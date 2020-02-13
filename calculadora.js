451a3eabea5dbbcc09b7c40378a4e0555108f5b1
Carolina Soares <m.carolina.soares1@gmail.com>
2020-02-12 21:15:13 -0300

    criei as funções para as operações

---
+++ calculadora.js
@@ -0,0 +1,32 @@
+function Somar() {
+	var num1 = document.getElementById("num1").value;
+	var num2 = document.getElementById("num2").value;
+	var somar = parseInt(num1) + parseInt(num2);
+	console.log(document.getElementById("resultado").innerHTML) = somar;
+}
+
+function Diminuir() {
+	var num1 = document.getElementById("num1").value;
+	var num2 = document.getElementById("num2").value;
+	var diminuir = parseInt(num1) - parseInt(num2);
+	document.getElementById("resultado").innerHTML = diminuir;
+}
+
+function Multiplicar() {
+	var num1 = document.getElementById("num1").value;
+	var num2 = document.getElementById("num2").value;
+	var multiplicar = parseInt(num1) * parseInt(num2);
+	document.getElementById("resultado").innerHTML = multiplicar;
+}
+
+function Dividir() {
+	var num1 = document.getElementById("num1").value;
+	var num2 = document.getElementById("num2").value;
+	var dividir = parseInt(num1) / parseInt(num2);
+	document.getElementById("resultado").innerHTML = dividir;
+}
+
+function Limpar() {
+  var = document.getElementById("");
+
+}
